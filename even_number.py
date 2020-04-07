f0d3b41fdd35252eff626c246aed42e5191b906c
KarolinaCzapla <61716321+KarolinaCzapla@users.noreply.github.com>
2020-04-07 16:02:35 +0200

    Delete even_number.py

--- even_number.py
+++
@@ -1,8 +0,0 @@
-# 7. Napisz program do rozpoznawania czy podane liczba jest parzysta czy nie.
-print("Program rozpoznaje czy podana liczba jest liczbą parzystą czy nieparzystą.")
-while True:
-    x=int(input("Podaj liczbę:"))
-    if x % 2 ==0:
-        print("Liczba jest parzysta!")
-    else:
-        print("Liczba jest nieparzysta!")
