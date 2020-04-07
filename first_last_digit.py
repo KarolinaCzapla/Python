74443466c2479dbbc8b8849a5f04ff478b8195a5
KarolinaCzapla <61716321+KarolinaCzapla@users.noreply.github.com>
2020-04-07 16:02:55 +0200

    Delete first_last_digit.py

--- first_last_digit.py
+++
@@ -1,5 +0,0 @@
-# 4. Napisz program, który poda pierwszą i ostatnią cyfrę podanej liczby
-while True:
-    number = input("Podaj liczbę:")
-    print( "Pierwsz cyfra to: ", number[0])
-    print("Ostatnia cyfra to: ", number[-1])
