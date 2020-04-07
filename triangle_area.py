66cd887c94ea5be1e5f2391678628255c63a4d32
KarolinaCzapla <61716321+KarolinaCzapla@users.noreply.github.com>
2020-04-07 16:03:28 +0200

    Delete triangle_area.py

--- triangle_area.py
+++
@@ -1,6 +0,0 @@
-# 3. Napisz program do obliczania pola powierzchni koła o zadanym promieniu (wyświetlając wzór i kolejne obliczenia)
-print("Program oblicza pole powierzchni koła o podanym promieniu.\nPole obliczane jest za pomocą wzoru:Pole = ((promien ** 2) * 3.14)")
-while True:
-    radius = float(input('Podaj dlugość promienia: '))
-    area = ((radius ** 2) * 3.14)
-    print("Pole wynosi %.2f " %(area))
