ab7c0fc4c07cceb8b37d0742ed76ccff77861ab7
KarolinaCzapla <61716321+KarolinaCzapla@users.noreply.github.com>
2020-04-07 16:01:41 +0200

    Delete div_number_3&5&7.py

--- div_number_3&5&7.py
+++
@@ -1,8 +0,0 @@
-# 9. Napisz program do sprawdzania czy liczba jest podzielne przez 3 i 5 i 7
-print("Program sprawdza czy liczba jest podzielne przez 3 i 5 i 7")
-while True:
-    x = int(input("Podaj liczbę: "))
-    if x % 3 == 0 and x % 5 == 0 and x % 7 == 0:
-        print("Liczba jest podzielna!")
-    else:
-        print("Liczba nie jest podzielna!")
